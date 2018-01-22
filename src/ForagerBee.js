419f471d36fd83d0725f72e56dd531eb937efced
Prasad Durgam <prasad.durgam@autodesk.com>
2018-01-22 11:50:58 +0530

    Done with it... wow.. nice feeling.. !

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,12 @@
-class ForagerBee {
-  // TODO..
+class ForagerBee extends HoneyMakerBee{
+  constructor(){
+    super();
+    this.job = 'find pollen';
+    this.canFly = true;
+    this.treasureChest = [];
+  }
+
+  forage(){
+    this.treasureChest.push('treasure');
+  }
 };
